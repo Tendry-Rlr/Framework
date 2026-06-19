9fe9c45cfd210c0b82fe7815f0db2cc8a6921a06
Tendrisoa Nantenaina <ralaiarimangatendry@gmail.com>
2026-06-19 10:32:57 +0300

    Sprint-1 Creation annotation et chargement des classes

--- src/main/java/route/RouteServlet.java
+++ src/main/java/route/RouteServlet.java
@@ -2,14 +2,26 @@ package route;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
+import annotation.controller.Controller;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import tools.Util;
 
 public class RouteServlet extends HttpServlet {
+    private List<String> classes;
+
+    public void init() throws ServletException {
+        String scan = this.getInitParameter("scanController");
+        
+        Util util = new Util();
+        classes = util.classes_annotes(scan, Controller.class);
+    }
+
     protected void doPost(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
         processRequest(req, res);
@@ -26,19 +38,25 @@ public class RouteServlet extends HttpServlet {
         String[] parts = lien.split("/");
         String texte = parts[parts.length - 1];
 
-        // verification si page
-        if (texte.contains(".html") || texte.contains(".jsp")) {
-            RequestDispatcher dispatcher = req.getServletContext().getNamedDispatcher("default");
-            dispatcher.forward(req, res);
+        // afficher les classes annotees
+        PrintWriter out = res.getWriter();
+        for (int i = 0; i < classes.size(); i++) {
+            out.println(classes.get(i));
         }
 
-        else {
-            if (texte.equals("App-Test")) {
-                texte = "";
-            }
-            PrintWriter out = res.getWriter();
-            out.println(texte);
-        }
+        // verification si page
+        // if (texte.contains(".html") || texte.contains(".jsp")) {
+        //     RequestDispatcher dispatcher = req.getServletContext().getNamedDispatcher("default");
+        //     dispatcher.forward(req, res);
+        // }
+
+        // else {
+        //     if (texte.equals("App-Test")) {
+        //         texte = "";
+        //     }
+        //     PrintWriter out = res.getWriter();
+        //     out.println(texte);
+        // }
 
     }
 }
---
+++ src/main/java/tools/Util.java
@@ -0,0 +1,113 @@
+package tools;
+
+import java.io.File;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+public class Util {
+    public List<String> classes_annotes(String nomPackage, Class<? extends Annotation> annotation) {
+        List<String> liste = new ArrayList<>();
+
+        for (Class<?> cls : this.scanner_packages(nomPackage)) {
+            if (cls.isAnnotationPresent(annotation)) {
+                liste.add(cls.getSimpleName());
+            }
+        }
+        return liste;
+    }
+
+    protected static String[] packages(String texte) {
+        String[] parts = texte.split(";");
+        return parts;
+    }
+
+    protected List<Class<?>> scanner_packages(String nomPackage) {
+        List<Class<?>> liste = new ArrayList<>();
+
+        List<String> temp = new ArrayList<>();
+        if (nomPackage.contains(";")) {
+            String[] parts = packages(nomPackage);
+            for (String pack : parts) {
+                try {
+                    List<String> valeurs = scan(pack);
+                    temp.addAll(valeurs);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            try {
+                temp = scan(nomPackage);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        Class<?> cls;
+        for (String nomClasse : temp) {
+            try {
+                cls = Class.forName(nomClasse);
+                liste.add(cls);
+
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return liste;
+    }
+
+    public List<String> scan(String nomDePackage) throws Exception {
+        List<String> classeNames = new ArrayList<>();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        String path = nomDePackage.replace('.', '/');
+        Enumeration<URL> resources = classLoader.getResources(path);
+
+        while (resources.hasMoreElements()) {
+            URL resource = resources.nextElement();
+            String protocol = resource.getProtocol();
+
+            // Cas 1 : Les classes sont dans un dossier classique (mode développement)
+            if ("file".equals(protocol)) {
+                File dir = new File(URLDecoder.decode(resource.getFile(), "UTF-8"));
+                if (dir.exists() && dir.isDirectory()) {
+                    File[] files = dir.listFiles((d, name) -> name.endsWith(".class"));
+                    if (files != null) {
+                        for (File file : files) {
+                            String className = file.getName().substring(0, file.getName().length() - 6);
+                            classeNames.add(nomDePackage + "." + className);
+                        }
+                    }
+                }
+            }
+            // Cas 2 : Les classes sont packagées dans un fichier .jar
+            else if ("jar".equals(protocol)) {
+                // On se connecte au fichier JAR
+                JarURLConnection jarConn = (JarURLConnection) resource.openConnection();
+                JarFile jar = jarConn.getJarFile();
+                Enumeration<JarEntry> entries = jar.entries();
+
+                while (entries.hasMoreElements()) {
+                    JarEntry entry = entries.nextElement();
+                    String entryName = entry.getName();
+
+                    // On cherche les fichiers qui commencent par notre chemin de package et
+                    // finissent par .class
+                    if (entryName.startsWith(path) && entryName.endsWith(".class")) {
+                        // On transforme le chemin du JAR (ex: route/RouteServlet.class) en nom de
+                        // classe (ex: route.RouteServlet)
+                        String className = entryName.replace('/', '.').substring(0, entryName.length() - 6);
+                        classeNames.add(className);
+                    }
+                }
+            }
+        }
+        return classeNames;
+    }
+}
